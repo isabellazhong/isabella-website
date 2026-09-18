2d7e70f469db658d113f449ec5fde0ce74487e06
Isabella Zhong <isabellazhongg@gmail.com>
2026-09-18 17:57:13 -0400

    fix deployment error

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,17 +1,5 @@
-import { EnvelopeSimple, FileText, GithubLogo, LinkSimple, LinkedinLogo } from "@phosphor-icons/react";
-import type { Icon } from "@phosphor-icons/react";
 import { ContactForm } from "../components/contact/ContactForm";
 import { PageHeader } from "../components/layout/PageHeader";
-import { contactLinks } from "../data/profile";
-import type { ContactLink } from "../types";
-
-const ICON_MAP: Record<ContactLink["icon"], Icon> = {
-  email: EnvelopeSimple,
-  github: GithubLogo,
-  linkedin: LinkedinLogo,
-  resume: FileText,
-  other: LinkSimple,
-};
 
 export default function ContactPage() {
   return (
